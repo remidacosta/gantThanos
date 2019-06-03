2c3f68859ff34b493a0292e8943e8f2ea1dccc3b
remidacosta <remidc2109@gmail.com>
2019-06-03 18:09:13 +0200

    création fenetre d'insertion ok

--- app/Http/Controllers/PersonneController.php
+++ app/Http/Controllers/PersonneController.php
@@ -28,13 +28,14 @@ class PersonneController extends Controller
     {
         $maPersonne = new PersonneDAO();
         $unePersonne = new Personne();
+        $resultatSnap = $this->snap();
 
         $unePersonne->setNom($request->input('Nom'));
         $unePersonne->setPrenom($request->input('Prenom'));
         $unePersonne->setSexe($request->get('Sexe'));
         $unePersonne->setNationalite($request->input('Nationalite'));
         $unePersonne->setDateNaissance($request->input('DateNaissance'));
-        $unePersonne->setEtat($this->snap());
+        $unePersonne->setEtat($resultatSnap);
 
 
         if(!$maPersonne->verifDoublon($unePersonne))
@@ -42,10 +43,7 @@ class PersonneController extends Controller
             $maPersonne->creationPersonne($unePersonne);
         }
 
-
-        $lesPersonnes = $maPersonne->getLesPersonnes();
-
-        return view('listePersonnes', compact( 'lesPersonnes'));
+        return view('insertionOK', compact( 'resultatSnap'));
     }
 
 
---
+++ resources/views/InsertionOK.blade.php
@@ -0,0 +1,15 @@
+<?php
+    if($resultatSnap == "Mort")
+    {
+        ?>
+        <h1>Vous êtes mort</h1>
+<?php
+    }
+    else if($resultatSnap == "Vivant")
+        {
+            ?>
+        <h1>Vous êtes vivant</h1>
+<?php
+        }
+?>
+
