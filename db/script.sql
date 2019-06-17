577f9c6d12a86f02df94f09f6c250a78b1563165
maximepouxberthe <maximepouxberthe@gmail.com>
2019-06-17 22:18:51 +0200

    modifications de la db et du .env pour l'hebergement

--- db/script.sql
+++ db/script.sql
@@ -1,8 +1,4 @@
-create database if not exists GantThanos character set utf8 collate utf8_unicode_ci;
-use GantThanos;
-grant all privileges on GantThanos.* to 'GantThanos_user'@'localhost' identified by '123123123';
-
-use GantThanos
+use gantthanos_gantthanos;
 
 drop table if exists users;
 
